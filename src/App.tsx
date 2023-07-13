32a4497fd482c9abaac975cdcbfa095498fe7259
iamzwq <876867644@qq.com>
2023-07-13 23:32:59 +0800

    feat: 添加 antd 全局配置的国际化修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,28 @@
-import { useEffect } from "react";
+import { useEffect, useMemo } from "react";
 import { App as AntdApp, ConfigProvider } from "antd";
 import { useGlobalStore } from "./stores";
 import zhCN from "antd/locale/zh_CN";
+import enUS from "antd/locale/en_US";
 import dayjs from "dayjs";
 import "dayjs/locale/zh-cn";
+import "dayjs/locale/en";
 import Router from "./router";
 import { i18next } from "~/i18n/config";
-
-dayjs.locale("zh-cn");
 
 const App = () => {
   const lang = useGlobalStore(state => state.lang);
   const colorPrimary = useGlobalStore(state => state.colorPrimary);
 
+  const locale = useMemo(() => (lang === "zh" ? zhCN : enUS), [lang]);
+
   useEffect(() => {
     i18next.changeLanguage(lang);
+    dayjs.locale(lang === "zh" ? "zh-cn" : "en");
   }, [lang]);
 
   return (
     <ConfigProvider
-      locale={zhCN}
+      locale={locale}
       theme={{
         token: {
           colorPrimary: colorPrimary,
